5147b9ad948d7baf13f40ccacf086c3ada8cbaae
Abhishek Kumar <68725300+coder-abhi07@users.noreply.github.com>
2025-03-27 17:14:15 +0530

    Delete smashIT - Copy - Copy directory

--- smashIT - Copy - Copy/smashIT - Copy/convert/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
-# Register your models here.
-
-admin.site.site_header = _("Administration")
-admin.site.site_title = _("Administration")
-admin.site.index_title = _("Welcome to the Admin Dashboard")
